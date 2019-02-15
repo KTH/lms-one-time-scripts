af60dfbc588fa1fb3a8b836c63630b61ee19e2d2
Carlos Saito <exacs@users.noreply.github.com>
2019-02-15 12:49:38 +0100

    Add a "start" script

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer')
+
+;(async function start () {
+  console.log(
+    'Welcome to the One Time Scripts',
+  )
+  const { answer } = await inquirer.prompt({
+    name: 'answer',
+    message: 'Choose the thing that you want to do today',
+    type: 'list',
+    choices: [
+      {name: 'Handle dead letters of a queue', value: './handleDeadLetter'},
+      {name: 'Get a list of users from UG', value: './getUgUsers'}
+    ]
+  })
+  require(answer)
+})()
